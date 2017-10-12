03d278504a11cecd12bdb9ba6f1a844be69f6454
Danny Weeks <danny@dannyweeks.com>
2017-10-12 05:20:54 +0100

    More randomness (#18)
    * Add word repository with random way of fetching a new words
    
    * Refactor random word method

--- src/AppBundle/Repository/WordRepository.php
+++ src/AppBundle/Repository/WordRepository.php
@@ -14,4 +14,31 @@ class WordRepository extends EntityRepository
     {
         return $this->findBy([], ['word' => 'ASC']);
     }
+
+    /**
+     * Get a random word that has not been assigned a date yet
+     *
+     * @return Word|null
+     */
+    public function getOneRandom()
+    {
+        $qb = $this->createQueryBuilder('w');
+
+        $count = $qb
+            ->select('COUNT(w)')
+            ->where('w.date IS NULL')
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        if (!$count) {
+            return null;
+        }
+
+        return $qb
+            ->select('w')
+            ->setFirstResult(rand(0, $count - 1))
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
